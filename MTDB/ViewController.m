92dc59282c557e20c1f8f07a453be7cfaebd4768
Tim <aunm123@yeah.net>
2016-01-16 09:52:52 +0800

    no message

---
+++ MTDB/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MTDB
+//
+//  Created by Tim on 16/1/16.
+//  Copyright © 2016年 zgltong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MTDB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MTDB
+//
+//  Created by Tim on 16/1/16.
+//  Copyright © 2016年 zgltong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
